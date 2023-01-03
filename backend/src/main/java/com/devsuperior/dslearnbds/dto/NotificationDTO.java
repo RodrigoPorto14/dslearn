d976cab5ec579447f1ef118fd08f131bcc8bf52e
rodri <rodrigo.porto1403@gmail.com>
2023-01-03 12:03:52 -0300

    Custom content for current user

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/dto/NotificationDTO.java
@@ -0,0 +1,53 @@
+package com.devsuperior.dslearnbds.dto;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import com.devsuperior.dslearnbds.entities.Notification;
+
+public class NotificationDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String text;
+	private Instant moment;
+	private boolean read;
+	private String route;	
+	private Long userId;
+	
+	public NotificationDTO() {}
+	
+	public NotificationDTO(Notification entity)
+	{
+		id = entity.getId();
+		text = entity.getText();
+		moment = entity.getMoment();
+		read = entity.isRead();
+		route = entity.getRoute();
+		userId = entity.getUser().getId();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public boolean isRead() {
+		return read;
+	}
+
+	public String getRoute() {
+		return route;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+}
